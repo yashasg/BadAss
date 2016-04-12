17996d26d0d95bcb43af3d0dd01a8942e9d3303a
Kun Cheng <kunchengsky@gmail.com>
2016-04-12 05:13:32 -0600

    Added a solution.

---
+++ Source/BadAss/LetterPuzzleConsole.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "LetterPuzzleConsole.generated.h"
+
+UCLASS()
+class BADASS_API ALetterPuzzleConsole : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PuzzleConsoleProperties")
+	FString theWord;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PuzzleConsoleProperties")
+	FString theCorrectWord;
+
+	UFUNCTION(BlueprintCallable, Category = "PuzzleConsoleFunctions")
+	void UpdateTheWord(FString i_newLetter);
+
+	// Sets default values for this actor's properties
+	ALetterPuzzleConsole();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	
+	
+};
---
+++ Source/BadAss/SliderConsole.cpp
@@ -0,0 +1,87 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BadAss.h"
+#include "SliderConsole.h"
+
+// Sets default values
+ASliderConsole::ASliderConsole()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASliderConsole::BeginPlay()
+{
+	Super::BeginPlay();
+	emptyIndex = 15;
+}
+
+// Called every frame
+void ASliderConsole::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+bool ASliderConsole::CheckPattern() {
+	/*if (sliderButtons.IsValidIndex(15) == false) {
+		return;
+	}*/
+	for (int i = 0; i < 15; ++i) {
+		if (sliderButtons[i]->currentPosition != sliderButtons[i]->correctPosition) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool ASliderConsole::MoveTile(int32 index) {
+	if (index < 0 || index > 15) {
+		return false;
+	}
+	// Check if this is next to the empty one
+	if (sliderButtons[index]->currentPosition % 4 == 0) {
+		if (sliderButtons[index]->currentPosition - 4 != sliderButtons[emptyIndex]->currentPosition
+			&& sliderButtons[index]->currentPosition + 4 != sliderButtons[emptyIndex]->currentPosition
+			&& sliderButtons[index]->currentPosition + 1 != sliderButtons[emptyIndex]->currentPosition) {
+			return false;
+		}
+	}
+	else if (sliderButtons[index]->currentPosition % 4 == 3) {
+		if (sliderButtons[index]->currentPosition - 4 != sliderButtons[emptyIndex]->currentPosition
+			&& sliderButtons[index]->currentPosition + 4 != sliderButtons[emptyIndex]->currentPosition
+			&& sliderButtons[index]->currentPosition - 1 != sliderButtons[emptyIndex]->currentPosition) {
+			return false;
+		}
+	}
+	else {
+		if (sliderButtons[index]->currentPosition - 4 != sliderButtons[emptyIndex]->currentPosition
+			&& sliderButtons[index]->currentPosition + 4 != sliderButtons[emptyIndex]->currentPosition
+			&& sliderButtons[index]->currentPosition - 1 != sliderButtons[emptyIndex]->currentPosition
+			&& sliderButtons[index]->currentPosition + 1 != sliderButtons[emptyIndex]->currentPosition) {
+			return false;
+		}
+	}
+
+	// switch the two tiles.
+	FVector tmpLoc = sliderButtons[emptyIndex]->GetActorLocation();
+	sliderButtons[emptyIndex]->SetActorLocation( sliderButtons[index]->GetActorLocation() );
+	sliderButtons[index]->SetActorLocation(tmpLoc);
+
+	int32 tmpPos = sliderButtons[emptyIndex]->currentPosition;
+	sliderButtons[emptyIndex]->currentPosition = sliderButtons[index]->currentPosition;
+	sliderButtons[index]->currentPosition = tmpPos;
+
+
+
+	//emptyIndex = index;
+
+	/*ASliderButton* pTmpBtn = sliderButtons[emptyIndex];
+	sliderButtons[emptyIndex] = sliderButtons[index];
+	sliderButtons[index] = pTmpBtn;
+	emptyIndex = index;*/
+
+	return true;
+}
